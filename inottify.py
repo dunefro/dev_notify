e54cab1917bf8b37b0da342796c9064769d98187
Vedant Pareek <dunefro@gmail.com>
2020-05-06 18:51:18 +0000

    Added recreate_objects

--- inottify.py
+++ inottify.py
@@ -54,7 +54,7 @@ def _read_config():
     return config_data
 
 def _check_object_exists(kind):
-    
+    # check whether the object exists or not
 
 def _check_patch_object(kind):
     #check patch here, return True otherwise False
